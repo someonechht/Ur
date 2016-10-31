3d3bd1a9ad2e739c3d1e0c8dac2d9d1a5bdc7847
Sebastian <sebastianbrandt@live.dk>
2016-10-31 15:18:06 +0100

    nah

--- Ur.py
+++ Ur.py
@@ -4,18 +4,20 @@ from math import *
 RATE = 1000
 SIZE = 800, 600
 centrum= SIZE [0]/2, SIZE [1]/2
-
 radius = 200
 
 def updateTask(root, canvas):
     canvas.delete(ALL)
     t = datetime.datetime.now()
     print(t)
-    drawTicks()
+    drawTicks(canvas)
     canvas.create_oval(centrum [0]-radius, centrum [1]-radius, centrum [0]+radius, centrum [1]+radius, width=2, fill='')
     root.after(RATE, updateTask, root, canvas)
+    pointer(canvas, t)
+
 
 def main():
+
     root = Tk()
     root.title("Min applikation")
     root.geometry(str(SIZE[0]) + 'x' + str(SIZE[1]))
@@ -24,12 +26,26 @@ def main():
     updateTask(root, canvas)
     root.mainloop()
 
-def drawTicks():
+def drawTicks(canvas):
     for sTick in range(1, 13):
         p = sTick/12
         rad = p * 2*pi
         x=200 * cos(rad)
         y=200 * sin(rad)
         canvas.create_line(centrum [0], centrum [1], x+centrum[0], y+centrum[1], width=5, fill='black')
+
+def pointer(canvas, t):
+
+    klok=t.hour
+
+    rad = klok*(1/12)*2*pi
+    x=200 * cos(rad)
+    y=200 * sin(rad)
+    canvas.create_line(centrum [0], centrum [1], x+centrum[0], y+centrum[1], width=20, fill='black')
+
+
+
+
+
 
 main()
