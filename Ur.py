a01c97f574d367ced00ca246e9ce243237cd87a0
Bruger <claus.huynh@yahoo.dk>
2016-10-31 13:42:55 +0100

    Chan

--- Ur.py
+++ Ur.py
@@ -4,12 +4,14 @@ from math import *
 
 RATE = 1000
 SIZE = 800, 600
+centrum= SIZE [0]/2, SIZE [1]/2
+radius = 200
 
 def updateTask(root, canvas):
     canvas.delete(ALL)
     t = datetime.datetime.now()
     print(t)
-    canvas.create_oval(25, 25, 80, 60, width=2, fill='')
+    canvas.create_oval(centrum [0]-radius, centrum [1]-radius, centrum [0]+radius, centrum [1]+radius, width=2, fill='')
     canvas.create_line(25, 25, 80, 60, width=5, fill='black')
     root.after(RATE, updateTask, root, canvas)
 
@@ -24,6 +26,7 @@ def main():
 
     updateTask(root, canvas)
     root.mainloop()
+
 
 
 main()
